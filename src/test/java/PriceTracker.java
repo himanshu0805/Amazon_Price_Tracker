67eaf1397cf0e50834abe56095a64d6055995cd4
himanshu0805 <33himanshuramawat@gmail.com>
2022-12-19 23:51:48 +0530

    getting Name and Price values in List

--- src/test/java/PriceTracker.java
+++ src/test/java/PriceTracker.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -12,36 +13,56 @@ public class PriceTracker {
 
 	public static void main(String[] args) {
 
-		String SearchKeyword = "Laptop";
+		String SearchKeyword = "earphone";
 
+		List<String> UrlList = getItemUrlWithSearch(SearchKeyword);
+		getProductDataFromUrl(UrlList);
+
+	}
+
+	public static List<String> getItemUrlWithSearch(String searchKeyword){
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://www.amazon.in/");
 		
 		WebElement search = driver.findElement(By.xpath("//input[contains(@id,'twotabsearchtextbox')]"));
-		search.sendKeys(SearchKeyword, Keys.ENTER);
+		search.sendKeys(searchKeyword, Keys.ENTER);
 		
 		List<WebElement> item_link = driver.findElements(By.xpath(
 				"//a[contains(@class,'a-link-normal ')]/ancestor::h2/a"));
 		
-		List<WebElement> item_link_Title = driver.findElements(By.xpath(
-				"//a[contains(@class,'a-link-normal ')]/ancestor::h2/a/span"));
+		List<String> item_link_Url = new ArrayList<String>();
 		
-		List<WebElement> item_link_Price = driver.findElements(By.xpath(
-				"//a[contains(@class,'a-link-normal ')]/ancestor::h2/a/following::span[@class='a-price-whole']"));
-
 		for(int i=0 ; i < item_link.size();i++) {
-			System.out.println("Item "+i);
-			System.out.println("item Links:- "+item_link.get(i).getAttribute("href"));
-			System.out.println("item Title:- "+item_link_Title.get(i).getText());
-			System.out.println("item Price:- "+item_link_Price.get(i).getText());
-			
-			System.out.println("\n\n");
+			System.out.println("item List extract:- "+item_link.get(i).getAttribute("href"));
+			item_link_Url.add(i, item_link.get(i).getAttribute("href"));
 		}
-		System.out.println("end loop");
-		driver.quit();
 		
+		for(int i=0 ; i < item_link_Url.size();i++) {
+			System.out.println("item List value:- "+item_link_Url.get(i));
+		}
+		
+		return item_link_Url;
+	}
+
+	public static void getProductDataFromUrl(List<String> UrlList) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		List<String> item_Name = new ArrayList<String>();
+		List<String> item_Price = new ArrayList<String>();
+		
+		for (int i = 0; i < UrlList.size(); i++) {
+			System.out.println("Url value:- " + UrlList.get(i));
+			driver.get(UrlList.get(i).toString());
+			item_Name.add(driver.findElement(By.xpath("//span[@id='productTitle']")).getText());
+			item_Price.add(driver.findElement(By.xpath("//span[@class='a-price-whole']")).getText().replace("\"", ""));
+			
+
+		}
+
+		driver.quit();
 	}
 
 }
