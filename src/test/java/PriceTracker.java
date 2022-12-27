b6d09cd87a6983f3e28720e1773839a8889d376c
himanshu0805 <33himanshuramawat@gmail.com>
2022-12-27 22:44:44 +0530

    Creating and Entering data in csv file

--- src/test/java/PriceTracker.java
+++ src/test/java/PriceTracker.java
@@ -1,4 +1,11 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -7,16 +14,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.opencsv.CSVWriter;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PriceTracker {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		String SearchKeyword = "earphone";
-
-		List<String> UrlList = getItemUrlWithSearch(SearchKeyword);
-		getProductDataFromUrl(UrlList);
+//		String SearchKeyword = "earphone";
+//		List<String> UrlList = getItemUrlWithSearch(SearchKeyword);
+//		getProductDataFromUrl(UrlList);
 
 	}
 
@@ -40,13 +48,16 @@ public class PriceTracker {
 		}
 		
 		for(int i=0 ; i < item_link_Url.size();i++) {
+			if(item_link_Url.get(i).trim().length()==0 && item_link_Url.get(i).trim()=="" ) {
+				item_link_Url.remove(i);
+			}
 			System.out.println("item List value:- "+item_link_Url.get(i));
 		}
 		
 		return item_link_Url;
 	}
 
-	public static void getProductDataFromUrl(List<String> UrlList) {
+	public static void getProductDataFromUrl(List<String> UrlList) throws IOException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		
@@ -58,11 +69,56 @@ public class PriceTracker {
 			driver.get(UrlList.get(i).toString());
 			item_Name.add(driver.findElement(By.xpath("//span[@id='productTitle']")).getText());
 			item_Price.add(driver.findElement(By.xpath("//span[@class='a-price-whole']")).getText().replace("\"", ""));
-			
-
 		}
+		String Filepath = new File("Result.csv").getCanonicalPath();
+		writeDataLineByLine(Filepath, UrlList, item_Name, item_Price);
 
 		driver.quit();
+	}
+	
+	public static void writeDataLineByLine(String filePath, List<String> Url , List<String> Name, List<String> Price)
+	{
+		//File path as specified
+	    File file = new File(filePath);
+	    
+	    //creating required file 
+	    try {
+			file.createNewFile();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    
+	    try {
+	    	//File Writer interface 
+	        FileWriter outputfile = new FileWriter(file);
+	        CSVWriter writer = new CSVWriter(outputfile);
+	  
+	        // adding Columns to csv file
+	        String[] header = { "Url", "Item_Name", "Item_Price", "Date_Checked" };
+	        writer.writeNext(header);
+	  
+	        //adding data to csv the first Time
+	        for(int i=0;i<Url.size();i++) {
+	        	// displaying current date and time
+	            Calendar cal = Calendar.getInstance();
+	            SimpleDateFormat simpleformat = new SimpleDateFormat("dd/MM/yyyy hh:mm:s");
+	            System.out.println("Today's date and time = "+simpleformat.format(cal.getTime()));
+	            // displaying date
+	            Format f = new SimpleDateFormat("MM/dd/yy");
+	            String strDate = f.format(new Date());
+	            System.out.println("Current Date = "+strDate);
+	        	String[] data1 = { Url.get(i), Name.get(i), Price.get(i) , strDate};
+		        writer.writeNext(data1);
+	        }
+	  
+	        // closing Interface
+	        writer.close();
+	    }
+	    catch (IOException e) {
+	        //checking for any IO Exception
+	        e.printStackTrace();
+	    }
 	}
 
 }
